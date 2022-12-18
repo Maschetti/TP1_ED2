95dae580778c1a51ea9ab2b16a4d059e4386596f
Maschetti <mateusvmaschietto@gmail.com>
2022-12-18 02:45:48 -0300

    chore: starting to opening the .bin

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include "Validation/validation.h"
 
 int main(int argc, char **argv) {
-  if(validation(argc, argv) == 1) {
-    printf("Rodando...\n");
+  FILE* arquivo;
+  if((arquivo = validation(argc, argv)) != NULL) {
+    printf("%p\n", arquivo);
+  }
+  else {
+    printf("ERRO: nao foi possivel abrir o arquivo\n");
   }
   return 1;
 }
