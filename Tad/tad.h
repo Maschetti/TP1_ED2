3ff2ec3ca82aa9bc3a4375e4724c7cc23dbd4210
Maschetti <mateusvmaschietto@gmail.com>
2022-12-18 02:46:30 -0300

    fix: putting files in the rigth place

--- Tad/tad.h
+++ Tad/tad.h
@@ -1,10 +1,13 @@
 #ifndef TAD_H
 #define TAD_H
+#include <stdio.h>
 
 typedef struct {
   int chave;
   long int dado1;
   char dado2[5000];
 } Registro;
+
+void imprimeRegistro(Registro registro);
 
 #endif
