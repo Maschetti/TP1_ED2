630c994c0fc011a58778ac096ba19c947b8ce377
Maschetti <mateusvmaschietto@gmail.com>
2022-12-19 23:00:12 -0300

    chore: add acessoSequencialIndexado into main

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
+#include "AcessoSequencialIndexado/acessoSequencialIndexado.h"
 #include "Validation/validation.h"
 
 int main(int argc, char **argv) {
   FILE* arquivo;
   if((arquivo = validation(argc, argv)) != NULL) {
-    printf("%p\n", arquivo);
+    int metodo = atoi(argv[1]);
+    int tamanhoArquivo = atoi(argv[2]);
+    Registro registroPesquisa;
+    registroPesquisa.chave = atoi(argv[4]);
+
+    if(metodo == 1) {
+      acessoSequencialIndexado(arquivo, tamanhoArquivo, &registroPesquisa);
+    }
+
+    imprimeRegistro(registroPesquisa);
   }
   else {
     printf("ERRO: nao foi possivel abrir o arquivo\n");
   }
+
+  fclose(arquivo);
   return 1;
 }
