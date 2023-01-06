7a10aa0b04210f04e4b14643c46b27047b64ae74
Maschetti <mateusvmaschietto@gmail.com>
2023-01-06 00:28:41 -0300

    chore: implementing insereIndice and creating insereNaPagina

--- ArvoreB/arvoreB.c
+++ ArvoreB/arvoreB.c
@@ -4,9 +4,15 @@ void pesquisaArvoreB() {
 
 }
 
+void insereNaPagina(Apontador paginaAtual, Indice item, Apontador paginaDireita) {
+  int naoAchouPosicao, k;
+  k = paginaAtual->items;
+  naoAchouPosicao = (k > 0) ? 1 : 0;
+}
+
 void insereIndice(Apontador paginaAtual, Indice item, int *cresceu, Indice *indiceRetorno, Apontador *paginaRetorno) {
   int i = 1, j;
-  Apontador auxiliar;
+  Apontador paginaAuxiliar;
 
   if(paginaAtual == NULL) {
     *cresceu = 1;
@@ -16,7 +22,7 @@ void insereIndice(Apontador paginaAtual, Indice item, int *cresceu, Indice *indi
     return ;
   }
 
-  while(i < paginaAtual->items && item.chave > paginaAtual->items->chave) i++;
+  while(i < paginaAtual->numeroItems && item.chave > paginaAtual->items[i].chave) i++;
 
   if(item.chave == paginaAtual->items[i - 1].chave) {
     *cresceu = 0;
@@ -25,30 +31,54 @@ void insereIndice(Apontador paginaAtual, Indice item, int *cresceu, Indice *indi
     return ;
   }
 
-  return ;
-}
+  if(item.chave < paginaAtual->items[i].chave) i--;
 
-void insereArvoreB(Apontador *arvore, Indice item) {
+  insereIndice(paginaAtual->paginasFilhas[i], item, cresceu, indiceRetorno, paginaRetorno);
+
+  if(!*cresceu) return ;
+
+  if(paginaAtual->numeroItems < 2 * M) {
+    insereNaPagina(paginaAtual, *indiceRetorno, *paginaRetorno);
+    *cresceu = 0;
+  }
+
+  paginaAuxiliar = malloc(sizeof(Apontador));
+  paginaAuxiliar->numeroItems = 0;
+  paginaAuxiliar->paginasFilhas[0] = NULL;
+
+  if(i < M + 1) {
+    insereNaPagina(paginaAuxiliar, paginaAtual->items[2 * M - 1], paginaAtual->paginasFilhas[2 * M]);
+    paginaAtual->numeroItems--;
+    insereNaPagina(paginaAtual, *indiceRetorno, *paginaRetorno);
+  }
+  else {
+    insereNaPagina(paginaAuxiliar, *indiceRetorno, *paginaRetorno);
+  }
+
+  return ;
+} 
+
+void insereNaArvoreB(Apontador *paginaRaiz, Indice item) {
   int cresceu;
   Indice indiceRetorno;
   Pagina *paginaRetorno, *paginaAuxiliar;
   
-  insereIndice(*arvore, item, &cresceu, &indiceRetorno, &paginaRetorno);
+  insereIndice(*paginaRaiz, item, &cresceu, &indiceRetorno, &paginaRetorno);
 
   if(cresceu) {
     paginaAuxiliar = malloc(sizeof(Pagina*));
     paginaAuxiliar->numeroItems = 1;
     paginaAuxiliar->items[0] = indiceRetorno;
     paginaAuxiliar->paginasFilhas[1] = paginaRetorno;
-    paginaAuxiliar->paginasFilhas[0] = *arvore;
-    *arvore = paginaAuxiliar;
+    paginaAuxiliar->paginasFilhas[0] = *paginaRaiz;
+    *paginaRaiz = paginaAuxiliar;
   }
 
   return ;
 }
 
 void arvoreB(FILE *arquivo, int tamanhoArquivo, Registro *registroPesquisa) {
-  Apontador arvore = NULL;
+  Apontador paginaRaiz = NULL;
   Registro auxiliar;
   Indice item;
 
@@ -56,7 +86,7 @@ void arvoreB(FILE *arquivo, int tamanhoArquivo, Registro *registroPesquisa) {
     fread(&auxiliar, sizeof(Registro), 1, arquivo);
     item.chave = auxiliar.chave;
     item.posicaoArquivo = i;
-    insereArvoreB(&arvore, item);
+    insereArvoreB(&paginaRaiz, item);
   }
 
   pesquisaArvoreB();
