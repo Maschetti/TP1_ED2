1448df952dabda899373ab5ff4fce6110644923c
Maschetti <mateusvmaschietto@gmail.com>
2023-01-18 23:54:40 -0300

    chore: create arvoreBStar specifics structs

---
+++ ArvoreBStar/arvoreBStar.h
@@ -0,0 +1,29 @@
+#ifndef ARVOREBSTAR_H
+#define ARVOREBSTAR_H
+#include "../Tad/tad.h"
+#include <stdlib.h>
+
+#define M 4
+
+typedef enum { Interna, Externa } IntExt;
+
+typedef struct Pagina *Apontador;
+
+typedef struct {
+  IntExt tipoPagina;
+
+  union {
+    struct {
+      int numeroIndices;
+      int chaves[2 * M];
+      Apontador paginasFilhas[2 * M + 1];
+    } U0;
+    
+    struct {
+      int numeroIndices;
+      Indice indices[2 * M];
+    } U1;
+  } UU;
+} Pagina;
+
+#endif
